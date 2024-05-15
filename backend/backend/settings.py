51f592f26d9aab43023743f3fb2ad6f8076a9a03
abhishek10600 <abhi10sheksharma@gmail.com>
2024-05-15 13:27:38 +0530

    ALLOWED HOST ADDED in settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    os.environ.get("ALLOWED_HOST_URL")
+]
 
 
 # Application definition
